0ed45e54a6469bbe4c287d2aff73769294f526d2
faizan rashid <faizan.rashid@eis.sg>
2023-05-26 11:16:20 +0500

    new changes

--- src/canvas/Camera.jsx
+++ src/canvas/Camera.jsx
@@ -1,9 +1,10 @@
-import React, { useRef } from "react";
+import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { easing } from "maath";
 import { useSnapshot } from "valtio";
 import state from "../store";
 
+// eslint-disable-next-line react/prop-types
 const Camera = ({ children }) => {
 	const group = useRef();
 	const snap = useSnapshot(state);
--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,13 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, Center } from "@react-three/drei";
 import Camera from "./Camera";
-import Backdrop from "./Backdrop";
+// import Backdrop from "./Backdrop";
 import Animator from "./Animator";
 
 const CanvasModel = () => {
 	return (
 		<Canvas>
-			<ambientLight intensity={0.5} />
+			<ambientLight />
 			<Environment preset="city" />
 			<Camera>
 				{/* <Backdrop /> */}
